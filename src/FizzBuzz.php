0a8f6fc73151d9d2ab48b82d34744ed633958bb0
marcware <Salvat@1982>
2020-02-04 17:20:56 +0100

    BLUE: Throw exception is number is string EXTRACT method

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -8,9 +8,18 @@ class FizzBuzz
 {
     public function print($number)
     {
+        $this->isInteger($number);
+        return $number;
+    }
+
+    /**
+     * @param $number
+     * @throws \Exception
+     */
+    protected function isInteger($number): void
+    {
         if (!is_numeric($number)) {
             throw new \Exception('Is not a number');
         }
-        return $number;
     }
 }
